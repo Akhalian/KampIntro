085f17cb903bda89f5ee662645d5d4868ba135a6
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-01-17 00:04:15 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety - tip güvenliği
+            // Do not repeat yourself - Kendini tekrarlama
+
+            // değer tutucu, alias
+            string katagoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            if (sistemeGirisYapmisMi)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+
+            Console.WriteLine(katagoriEtiketi);
+
+        }
+    }
+}
