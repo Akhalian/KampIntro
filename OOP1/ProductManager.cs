6d07957cd9557c0c12c9ff756192cb6978585794
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-01-20 23:50:29 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            System.Console.WriteLine(product.ProductName + " eklendi.");
+
+        }
+        public void Update(Product product)
+        {
+            System.Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public void Topla(int sayi1, int sayi2)
+        {
+            System.Console.WriteLine(sayi1 + sayi2);
+        }
+    }
+}
