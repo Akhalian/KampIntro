760f59fec85a4b4c4e44a242fc14b9da3116c757
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-01-23 21:12:47 +0300

    day 4 task 5 edit

--- MyDictionary/MyDict.cs
+++ MyDictionary/MyDict.cs
@@ -4,47 +4,47 @@ using System.Text;
 
 namespace MyDictionary
 {
-    class MyDict<Id, Name>
+    class MyDict<Key, Value>
     {
-        Id[] id;
-        Name[] name;
+        Key[] key;
+        Value[] value;
         public MyDict()
         {
-            id = new Id[0];
-            name = new Name[0];
+            key = new Key[0];
+            value = new Value[0];
         }
 
-        Id[] tempid;
-        Name[] tempname;
+        Key[] tempkey;
+        Value[] tempvalue;
 
-        public void Add(Id ids, Name names)
+        public void Add(Key keys, Value values)
         {
-            tempid = id;
-            tempname = name;
-            id = new Id[id.Length + 1];
-            name = new Name[name.Length+1];
+            tempkey = key;
+            tempvalue = value;
+            key = new Key[key.Length + 1];
+            value = new Value[value.Length+1];
             
-            for (int i = 0; i < tempid.Length; i++)
+            for (int i = 0; i < tempkey.Length; i++)
             {
-                id[i] = tempid[i];
+                key[i] = tempkey[i];
             }
 
-            for (int i = 0; i < tempname.Length; i++)
+            for (int i = 0; i < tempvalue.Length; i++)
             {
-                name[i] = tempname[i];
+                value[i] = tempvalue[i];
             }
 
-            id[id.Length - 1] = ids;
-            name[name.Length - 1] = names;
+            key[key.Length - 1] = keys;
+            value[value.Length - 1] = values;
 
 
         }
 
         public void GetAll()
         {
-            for (int i = 0; i < id.Length; i++)
+            for (int i = 0; i < key.Length; i++)
             {
-                Console.WriteLine("Ad Soyad: " + id[i] + " " + name[i]);
+                Console.WriteLine("Ad Soyad: " + key[i] + " " + value[i]);
             }
         }
     }
