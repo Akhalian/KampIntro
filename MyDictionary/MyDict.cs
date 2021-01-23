926004cb2945d689e07e44c2c0538da067f0176f
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-01-23 20:52:28 +0300

    Day 4 Task 5

---
+++ MyDictionary/MyDict.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDict<Id, Name>
+    {
+        Id[] id;
+        Name[] name;
+        public MyDict()
+        {
+            id = new Id[0];
+            name = new Name[0];
+        }
+
+        Id[] tempid;
+        Name[] tempname;
+
+        public void Add(Id ids, Name names)
+        {
+            tempid = id;
+            tempname = name;
+            id = new Id[id.Length + 1];
+            name = new Name[name.Length+1];
+            
+            for (int i = 0; i < tempid.Length; i++)
+            {
+                id[i] = tempid[i];
+            }
+
+            for (int i = 0; i < tempname.Length; i++)
+            {
+                name[i] = tempname[i];
+            }
+
+            id[id.Length - 1] = ids;
+            name[name.Length - 1] = names;
+
+
+        }
+
+        public void GetAll()
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                Console.WriteLine("Ad Soyad: " + id[i] + " " + name[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<string, string> myDict = new MyDict<string, string>();
+            myDict.Add("1234", "Burak");
+
+            myDict.GetAll();
+
+            Console.WriteLine("Burada bitiyor ilk dict");
+
+            myDict.Add("5678", "Murat");
+
+            myDict.GetAll();
+
+
+
+
+        }
+    }
+}
