a06b71e87b4254bd45175e07e88d4c2bb39c4f90
Burak Baskın <2011404076@ogr.mehmetakif.edu.tr>
2021-02-07 12:42:24 +0300

    07.02.2021 exercises.

---
+++ Project4DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project4DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
